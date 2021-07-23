d723806b1211a0042e858eeb7bc0db889dc01fa2
danieltyukov <60662998+danieltyukov@users.noreply.github.com>
2021-07-23 16:21:06 +0300

    preparation of data

--- main.py
+++ main.py
@@ -9,5 +9,25 @@ from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.models import Sequential
 
 crypto_currency = 'BTC'
-against = 'USD'
+against_currency = 'USD'
+
+start = dt.datetime(2018, 1, 1)
+end = dt.datetime.now()
+
+data = web.DataReader(f'{crypto_currency}-{against_currency}', 'yahoo',start, end)
+
+# print(data.head())
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1,1))
+
+predict_days = 60
+
+x_train, y_train = [], []
+
+for x in range(predict_days, len(scaled_data)):
+    x_train.append(scaled_data[x-predict_days:x, 0])
+    y_train.append(scaled_data[x, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
 
